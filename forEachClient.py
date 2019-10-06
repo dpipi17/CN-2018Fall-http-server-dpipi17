752a1d9389a26cb42bb4bcd3552eb34406861fd2
dpipi17 <48618124+dpipi17@users.noreply.github.com>
2019-10-06 19:32:43 +0400

    keep-alive added and done

--- forEachClient.py
+++ forEachClient.py
@@ -32,117 +32,125 @@ class WorkerForClient(threading.Thread):
     def run(self):
 
         with self.conn:
-            request = b''
             while True:
-                currRequest = self.conn.recv(1024)
-                request += currRequest
-                if len(currRequest) < 1024:
+                request = b''
+                while True:
+                    try:
+                        self.conn.settimeout(5)
+                        currRequest = self.conn.recv(1024)
+                    except Exception:
+                        break
+                    request += currRequest
+                    if len(currRequest) < 1024:
+                        break
+
+                if request == b'':
                     break
-
-            decodedData = request.decode()
-            arr = decodedData.split('\r\n')
-
-            methodArr = arr[0].split(' ')
-            method = methodArr[0]
-            myFile = methodArr[1].split('?')[0]
-            myFile = myFile.replace('%20', ' ')
-
-            date = datetime.now()
-            date_time = date.strftime("%a %b %-d %H:%M:%S %Y")
-            userIp = self.addr[0]
-            domain = ''
-            userAgent = ''
-            connection = ''
-            rangeVal = ''
-            etagVal = ''
-
-            for header in arr:
-                if header.find('Host:') != -1:
-                    domain = header[header.find('Host:') + 6:]
-                    domain = domain.split(':')[0]
-                elif header.find('User-Agent:') != -1:
-                    userAgent = header[header.find('User-Agent:') + 12:]
-                elif header.find('Connection:') != -1:
-                    connection = header[header.find('Connection:') + 12:]
-                elif header.find('Range: bytes=') != -1:
-                    rangeVal = header[header.find('Range: bytes=') + 13:]
-                elif header.find('host:') != -1:
-                    domain = header[header.find('host:') + 6:]
-                    domain = domain.split(':')[0]
-                elif header.find('If-None-Match:') != -1:
-                    etagVal = header[header.find('If-None-Match:') + 15:]
-            
-            
-            header = ''
-            currResponse = b''
-            if not domain in self.checkDomain[(self.ip, self.port)]:
-                header = 'HTTP/1.1 404 Not Found\r\n'
-                currResponse = 'REQUESTED DOMAIN NOT FOUND\r\n'
-                header += 'Content-Length: ' + str(len(currResponse)) + '\n\n'
                 
-                writeIntoLog(self.log + '/error.log', date_time, userIp, domain, myFile, '404', str(len(currResponse)), userAgent)
-                self.conn.send((header + currResponse).encode())
-                return
+                decodedData = request.decode()
+                arr = decodedData.split('\r\n')
+
+                methodArr = arr[0].split(' ')
+                method = methodArr[0]
+                myFile = methodArr[1].split('?')[0]
+                myFile = myFile.replace('%20', ' ')
+
+                date = datetime.now()
+                date_time = date.strftime("%a %b %-d %H:%M:%S %Y")
+                userIp = self.addr[0]
+                domain = ''
+                userAgent = ''
+                connection = ''
+                rangeVal = ''
+                etagVal = ''
+
+                for header in arr:
+                    if header.find('Host:') != -1:
+                        domain = header[header.find('Host:') + 6:]
+                        domain = domain.split(':')[0]
+                    elif header.find('User-Agent:') != -1:
+                        userAgent = header[header.find('User-Agent:') + 12:]
+                    elif header.find('Connection:') != -1:
+                        connection = header[header.find('Connection:') + 12:]
+                    elif header.find('Range: bytes=') != -1:
+                        rangeVal = header[header.find('Range: bytes=') + 13:]
+                    elif header.find('host:') != -1:
+                        domain = header[header.find('host:') + 6:]
+                        domain = domain.split(':')[0]
+                    elif header.find('If-None-Match:') != -1:
+                        etagVal = header[header.find('If-None-Match:') + 15:]
+                
+                
+                header = ''
+                currResponse = b''
+                if not domain in self.checkDomain[(self.ip, self.port)]:
+                    header = 'HTTP/1.1 404 Not Found\r\n'
+                    currResponse = 'REQUESTED DOMAIN NOT FOUND\r\n'
+                    header += 'Content-Length: ' + str(len(currResponse)) + '\n\n'
+                    
+                    writeIntoLog(self.log + '/error.log', date_time, userIp, domain, myFile, '404', str(len(currResponse)), userAgent)
+                    self.conn.send((header + currResponse).encode())
+                    continue
 
 
-            statusCode = '200'
-            try:
-                mfile = open(self.documentRoots[domain] + myFile,'rb')
-                currResponse = mfile.read()
-                mfile.close()
+                statusCode = '200'
+                try:
+                    mfile = open(self.documentRoots[domain] + myFile,'rb')
+                    currResponse = mfile.read()
+                    mfile.close()
 
-                if rangeVal != '':
-                    currArr = rangeVal.split('-')
-                    statusCode = '206'
-                    if currArr[1] != '':
-                        if int(currArr[0]) > int(currArr[1]):
-                            header = 'HTTP/1.1 416 Requested Range Not Satisfiable status\n\n'
-                            writeIntoLog(self.log + '/error.log', date_time, userIp, domain, myFile, '416', str(len(currResponse)), userAgent)
-                            self.conn.send(header.encode())
-                            return
+                    if rangeVal != '':
+                        currArr = rangeVal.split('-')
+                        statusCode = '206'
+                        if currArr[1] != '':
+                            if int(currArr[0]) > int(currArr[1]):
+                                header = 'HTTP/1.1 416 Requested Range Not Satisfiable status\n\n'
+                                writeIntoLog(self.log + '/error.log', date_time, userIp, domain, myFile, '416', str(len(currResponse)), userAgent)
+                                self.conn.send(header.encode())
+                                continue
 
-                        currResponse = currResponse[int(currArr[0]):int(currArr[1]) + 1]
-                    else:
-                        currResponse = currResponse[int(currArr[0]):]
+                            currResponse = currResponse[int(currArr[0]):int(currArr[1]) + 1]
+                        else:
+                            currResponse = currResponse[int(currArr[0]):]
 
 
-                mime = magic.Magic(mime=True)
-                mimetype = str(mime.from_file(self.documentRoots[domain] + myFile))
+                    mime = magic.Magic(mime=True)
+                    mimetype = str(mime.from_file(self.documentRoots[domain] + myFile))
 
-                #to have css in html
-                if userAgent.find('Chrome') != -1 or userAgent.find('Mozilla') != -1:
-                    if myFile.endswith('.css'):
-                        mimetype = 'text/css'
+                    #to have css in html
+                    if userAgent.find('Chrome') != -1 or userAgent.find('Mozilla') != -1:
+                        if myFile.endswith('.css'):
+                            mimetype = 'text/css'
 
-                if etagVal == str(currResponse.__hash__()):
-                    header = 'HTTP/1.1 304 Not Modified \n\n'
-                    writeIntoLog(self.log + '/' + domain + '.log', date_time, userIp, domain, myFile, '304', '0', userAgent)
-                    self.conn.send(header.encode())
-                    return
+                    if etagVal == str(currResponse.__hash__()):
+                        header = 'HTTP/1.1 304 Not Modified \n\n'
+                        writeIntoLog(self.log + '/' + domain + '.log', date_time, userIp, domain, myFile, '304', '0', userAgent)
+                        self.conn.send(header.encode())
+                        continue
 
-                header = 'HTTP/1.1 ' + statusCode + ' OK\r\n'
-                header += 'Host:' + domain + '\r\n'
-                header += 'Server: My_Server\r\n'
-                header += 'Date: ' + date_time + '\r\n'
-                header += 'Content-Length: ' + str(len(currResponse)) + '\r\n'
-                header += 'Content-Type: ' + mimetype + '\r\n'
-                header += 'ETag: ' + str(currResponse.__hash__()) + ' \r\n'
-                header += 'Accept-Ranges: bytes\r\n'
-                header += 'Connection: keep-alive\r\n'
-                header += 'Keep-Alive: 5\n\n'
+                    header = 'HTTP/1.1 ' + statusCode + ' OK\r\n'
+                    header += 'Host:' + domain + '\r\n'
+                    header += 'Server: My_Server\r\n'
+                    header += 'Date: ' + date_time + '\r\n'
+                    header += 'Content-Length: ' + str(len(currResponse)) + '\r\n'
+                    header += 'Content-Type: ' + mimetype + '\r\n'
+                    header += 'ETag: ' + str(currResponse.__hash__()) + ' \r\n'
+                    header += 'Accept-Ranges: bytes\r\n'
+                    header += 'Connection: keep-alive\r\n'
+                    header += 'Keep-Alive: 5\n\n'
 
-            except Exception as e:
-                print(e)
- 
-            
-            writeIntoLog(self.log + "/" + domain + ".log", date_time, userIp, domain, myFile, statusCode, str(len(currResponse)), userAgent)
-            if method == 'GET':
-                response = header.encode()
-                response += currResponse
-            elif method == 'HEAD':
-                response = header.encode()
+                except Exception as e:
+                    print(e)
+    
+                
+                writeIntoLog(self.log + "/" + domain + ".log", date_time, userIp, domain, myFile, statusCode, str(len(currResponse)), userAgent)
+                if method == 'GET':
+                    response = header.encode()
+                    response += currResponse
+                elif method == 'HEAD':
+                    response = header.encode()
 
-            self.conn.sendall(response)
+                self.conn.sendall(response)
 
 
 
